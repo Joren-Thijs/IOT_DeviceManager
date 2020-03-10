82492e55dc974f00d22a52cd22c5723b2864b7fe
Joren Thijs <41837971+Joren-Thijs@users.noreply.github.com>
2020-03-11 00:29:36 +0100

    created mqtt wrapper class

---
+++ IOT_Thermostat.IOT/src/mqtt_client.py
@@ -0,0 +1,26 @@
+import paho.mqtt.client as mqtt
+from time import sleep
+
+
+class MQTTClient:
+    def __init__(self):
+        # create mqtt client
+        self.client = mqtt.Client()
+        self.client.on_connect = self.on_connect
+        self.client.on_message = self.on_message
+
+        # connect to mqtt broker. connect(ip adress, port, keep alive time)
+        self.client.connect("169.254.39.51", 1883, 60)
+
+    # The callback for when the client receives a CONNACK response from the server.
+    def on_connect(self, client, userdata, flags, rc):
+
+        print("Connected with result code "+str(rc))
+
+        # Subscribing in on_connect() means that if we lose the connection and
+        # reconnect then subscriptions will be renewed.
+        self.client.subscribe("#")
+
+    # The callback for when a PUBLISH message is received from the server.
+    def on_message(self, client, userdata, msg):
+        print(msg.topic+" "+str(msg.payload))
