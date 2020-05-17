9b1d437f894b68a4c0ff9fe59b732708fdb9dfe0
Joren Thijs <41837971+Joren-Thijs@users.noreply.github.com>
2020-05-17 10:46:33 +0200

    Added MQTT Client Service

---
+++ IOT_Thermostat.API/Services/IDeviceClient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IOT_Thermostat.API.Services
+{
+    public interface IDeviceClient
+    {
+        Task StartDeviceClientAsync(CancellationToken stoppingToken);
+    }
+}
--- IOT_Thermostat.API/Startup.cs
+++ IOT_Thermostat.API/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IOT_Thermostat.API.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -25,6 +26,8 @@ namespace IOT_Thermostat.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddHostedService<MQTTHostService>();
+            services.AddScoped<IDeviceClient, MQTTCLientService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
