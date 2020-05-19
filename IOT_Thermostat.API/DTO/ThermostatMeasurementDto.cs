e6915194491ad95ac6a9ee1f84fd18f829f35036
Joren Thijs <41837971+Joren-Thijs@users.noreply.github.com>
2020-05-20 00:03:15 +0200

    added automapper

---
+++ IOT_Thermostat.API/DTO/ThermostatMeasurementDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IOT_Thermostat.API.Models;
+
+namespace IOT_Thermostat.API.DTO
+{
+    public class ThermostatMeasurementDto
+    {
+        public string Id { get; set; }
+        public float Temperature { get; set; }
+        public float SetPoint { get; set; }
+        public IDeviceStatus Status { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public string DeviceId { get; set; }
+    }
+}
