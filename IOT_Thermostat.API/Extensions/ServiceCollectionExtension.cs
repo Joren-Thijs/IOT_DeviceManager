ed2c36eae8531a251e7507012d98b1e688d20965
Joren Thijs <41837971+Joren-Thijs@users.noreply.github.com>
2020-05-23 11:35:13 +0200

    added newtonsoft and xml serializers

--- IOT_Thermostat.API/Extensions/ServiceCollectionExtension.cs
+++ IOT_Thermostat.API/Extensions/ServiceCollectionExtension.cs
@@ -4,11 +4,31 @@ using IOT_Thermostat.API.Repositories;
 using IOT_Thermostat.API.Services;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json.Serialization;
 
 namespace IOT_Thermostat.API.Extensions
 {
     public static class ServiceCollectionExtension
     {
+        public static IServiceCollection AddControllersWithInputOutputFormatters(this IServiceCollection services)
+        {
+            services
+                .AddControllers()
+                .AddNewtonsoftJson(
+                    setupAction =>
+                    {
+                        setupAction.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                    })
+                .AddXmlDataContractSerializerFormatters();
+            return services;
+        }
+
+        public static IServiceCollection AddRepositoryService(this IServiceCollection services)
+        {
+            services.AddTransient<IDeviceRepository, DeviceInMemoryRepository>();
+            return services;
+        }
+
         public static IServiceCollection AddDeviceClientHostedService(this IServiceCollection services)
         {
             services.AddTransient<IDeviceClient, MqttDeviceClient>();
@@ -17,12 +37,6 @@ namespace IOT_Thermostat.API.Extensions
             {
                 return serviceProvider.GetService<DeviceClientService>();
             });
-            return services;
-        }
-
-        public static IServiceCollection AddRepositoryService(this IServiceCollection services)
-        {
-            services.AddTransient<IDeviceRepository, DeviceInMemoryRepository>();
             return services;
         }
     }
