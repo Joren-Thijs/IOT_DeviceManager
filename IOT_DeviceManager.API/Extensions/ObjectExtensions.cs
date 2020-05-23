183655cb6fb51815d3ad43f4f94da287f05a42e1
Joren Thijs <41837971+Joren-Thijs@users.noreply.github.com>
2020-05-23 16:03:55 +0200

    removed unused extension method

--- IOT_DeviceManager.API/Extensions/ObjectExtensions.cs
+++ IOT_DeviceManager.API/Extensions/ObjectExtensions.cs
@@ -5,15 +5,6 @@ namespace IOT_DeviceManager.API.Extensions
 {
     public static class ObjectExtensions
     {
-        public static object GetDefault(this object obj, Type type)
-        {
-            if (type.IsValueType)
-            {
-                return Activator.CreateInstance(type);
-            }
-            return null;
-        }
-
         public static string SerializeJson(this object obj, Formatting formatting = Formatting.Indented) => JsonConvert.SerializeObject(obj, formatting);
     }
 }
