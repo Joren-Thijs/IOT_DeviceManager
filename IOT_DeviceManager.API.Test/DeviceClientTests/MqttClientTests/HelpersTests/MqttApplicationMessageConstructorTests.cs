d7f2b70d63092744ee9c2051b1647c339444e42e
Joren Thijs <41837971+Joren-Thijs@users.noreply.github.com>
2020-05-23 21:13:57 +0200

    added testing for rpc answer

--- IOT_DeviceManager.API.Test/DeviceClientTests/MqttClientTests/HelpersTests/MqttApplicationMessageConstructorTests.cs
+++ IOT_DeviceManager.API.Test/DeviceClientTests/MqttClientTests/HelpersTests/MqttApplicationMessageConstructorTests.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using FluentAssertions;
 using IOT_DeviceManager.API.DeviceClient.MqttClient.Helpers;
 using IOT_DeviceManager.API.Entity.Device;
 using IOT_DeviceManager.API.Entity.ThermostatDevice;
+using IOT_DeviceManager.API.Extensions;
 using NUnit.Framework;
 
 namespace IOT_DeviceManager.API.Test.DeviceClientTests.MqttClientTests.HelpersTests
@@ -35,6 +37,23 @@ namespace IOT_DeviceManager.API.Test.DeviceClientTests.MqttClientTests.HelpersTe
             var topic = MqttApplicationMessageConstructor.GetSetDeviceStatusRpcTopic(device);
             var correctTopic = $"thermostat.{device.Id}.cmd.status";
             Assert.AreEqual(topic, correctTopic);
+        }
+
+        [Test]
+        public void CheckDeviceStatusCanBeConstructedFromRpcAnswer_ReturnsTrue()
+        {
+            var device = new Device
+            {
+                Id = "1"
+            };
+            var status = new DeviceStatus
+            {
+                OnStatus = true
+            };
+            var statusString = status.SerializeJson();
+            var statusStringBytes = Encoding.ASCII.GetBytes(statusString);
+            var newStatus = MqttApplicationMessageConstructor.GetDeviceStatusFromRcpAnswer(device, statusStringBytes);
+            newStatus.Should().BeEquivalentTo(status);
         }
     }
 }
