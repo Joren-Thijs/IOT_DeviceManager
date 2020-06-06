e0bc36781b27058974fbfed0b6aa9922d3b48e55
Joren Thijs <41837971+Joren-Thijs@users.noreply.github.com>
2020-06-06 21:20:21 +0200

    created xamarin projects

---
+++ IOT_DeviceManager.APP/IOT_DeviceManager.APP/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using IOT_DeviceManager.APP.Services;
+using IOT_DeviceManager.APP.Views;
+
+namespace IOT_DeviceManager.APP
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ IOT_DeviceManager.APP/IOT_DeviceManager.APP/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using IOT_DeviceManager.APP.Models;
+
+namespace IOT_DeviceManager.APP.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
